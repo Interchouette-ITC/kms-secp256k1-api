1c8e533e397358f3c71af1a631342511ddac9749
gRoussac <gRoussac@users.noreply.github.com>
2025-08-20 23:46:05 +0200

    doc\n

---
+++ docs/api-rust/type.impl/kms_secp256k1_api/wasm_loader/struct.WasmInstance.js
@@ -0,0 +1,9 @@
+(function() {
+    var type_impls = Object.fromEntries([["kms_secp256k1_api",[]]]);
+    if (window.register_type_impls) {
+        window.register_type_impls(type_impls);
+    } else {
+        window.pending_type_impls = type_impls;
+    }
+})()
+//{"start":55,"fragment_lengths":[24]}
