afa5398e9a8ceae0ffd73e87de3ce7daa47770f0
gRoussac <gRoussac@users.noreply.github.com>
2026-07-30 18:15:06 +0000

    Deploy documentation for dev

--- trait.impl/core/marker/trait.Copy.js
+++ trait.impl/core/marker/trait.Copy.js
@@ -1,9 +1,9 @@
 (function() {
-    var implementors = Object.fromEntries([["kms_secp256k1_api",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.92.0/core/marker/trait.Copy.html\" title=\"trait core::marker::Copy\">Copy</a> for <a class=\"enum\" href=\"kms_secp256k1_api/config/enum.HashType.html\" title=\"enum kms_secp256k1_api::config::HashType\">HashType</a>"]]]]);
+    const implementors = Object.fromEntries([["kms_secp256k1_api",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.97.1/core/marker/trait.Copy.html\" title=\"trait core::marker::Copy\">Copy</a> for <a class=\"enum\" href=\"kms_secp256k1_api/config/enum.HashType.html\" title=\"enum kms_secp256k1_api::config::HashType\">HashType</a>",0]]]]);
     if (window.register_implementors) {
         window.register_implementors(implementors);
     } else {
         window.pending_implementors = implementors;
     }
 })()
-//{"start":57,"fragment_lengths":[304]}
+//{"start":59,"fragment_lengths":[306]}
--- trait.impl/utoipa/trait.OpenApi.js
+++ trait.impl/utoipa/trait.OpenApi.js
@@ -1,9 +1,9 @@
 (function() {
-    var implementors = Object.fromEntries([["kms_secp256k1_api",[["impl OpenApi for <a class=\"struct\" href=\"kms_secp256k1_api/routes/struct.ApiDoc.html\" title=\"struct kms_secp256k1_api::routes::ApiDoc\">ApiDoc</a>"]]]]);
+    const implementors = Object.fromEntries([["kms_secp256k1_api",[["impl OpenApi for <a class=\"struct\" href=\"kms_secp256k1_api/routes/struct.ApiDoc.html\" title=\"struct kms_secp256k1_api::routes::ApiDoc\">ApiDoc</a>",0]]]]);
     if (window.register_implementors) {
         window.register_implementors(implementors);
     } else {
         window.pending_implementors = implementors;
     }
 })()
-//{"start":57,"fragment_lengths":[179]}
+//{"start":59,"fragment_lengths":[181]}
