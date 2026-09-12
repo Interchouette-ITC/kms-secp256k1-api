2c9bbb2b3b873637be102ca06c091e4918c2a8d5
gRoussac <gRoussac@users.noreply.github.com>
2026-09-12 13:58:42 +0000

    Deploy documentation for dev

--- trait.impl/core/error/trait.Error.js
+++ trait.impl/core/error/trait.Error.js
@@ -1,5 +1,5 @@
 (function() {
-    const implementors = Object.fromEntries([["kms_secp256k1_api",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.97.1/core/error/trait.Error.html\" title=\"trait core::error::Error\">Error</a> for <a class=\"enum\" href=\"kms_secp256k1_api/error/enum.KmsError.html\" title=\"enum kms_secp256k1_api::error::KmsError\">KmsError</a>",0]]]]);
+    const implementors = Object.fromEntries([["kms_secp256k1_api",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.98.1/core/error/trait.Error.html\" title=\"trait core::error::Error\">Error</a> for <a class=\"enum\" href=\"kms_secp256k1_api/error/enum.KmsError.html\" title=\"enum kms_secp256k1_api::error::KmsError\">KmsError</a>",0]]]]);
     if (window.register_implementors) {
         window.register_implementors(implementors);
     } else {
--- trait.impl/core/marker/trait.Copy.js
+++ trait.impl/core/marker/trait.Copy.js
@@ -1,5 +1,5 @@
 (function() {
-    const implementors = Object.fromEntries([["kms_secp256k1_api",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.97.1/core/marker/trait.Copy.html\" title=\"trait core::marker::Copy\">Copy</a> for <a class=\"enum\" href=\"kms_secp256k1_api/config/enum.HashType.html\" title=\"enum kms_secp256k1_api::config::HashType\">HashType</a>",0]]]]);
+    const implementors = Object.fromEntries([["kms_secp256k1_api",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.98.1/core/marker/trait.Copy.html\" title=\"trait core::marker::Copy\">Copy</a> for <a class=\"enum\" href=\"kms_secp256k1_api/config/enum.HashType.html\" title=\"enum kms_secp256k1_api::config::HashType\">HashType</a>",0]]]]);
     if (window.register_implementors) {
         window.register_implementors(implementors);
     } else {
